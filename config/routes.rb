60f2297d8d262572b0278f30efebabba7bc8115a
Elton Santos <eltonaxl@hotmail.com>
2022-03-19 23:36:06 -0300

    Commiting new changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   
   root to: 'pages#home'
+  
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
